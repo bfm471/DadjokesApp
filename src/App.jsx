54b3995f1f230b56e41163cff7fa431438eaefbf
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-05-17 20:38:25 +0300

    Latauksia

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LoginForm from './login'
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+    <div className="App">
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
-
-export default App
